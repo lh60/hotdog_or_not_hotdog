cdadda4913be0a82d9c793f6cc1d29a9daeb9c84
Lily <lh600@scarletmail.rutgers.edu>
2021-11-29 20:06:45 -0500

    Initial Commit

---
+++ hotdog_or_not_hotdog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotdog_or_not_hotdog
+//
+//  Created by Lily on 11/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
